f86dc02e7311b602c489d014c7b755ec70651661
RaulRamirezzz <za210110207@zapopan.tecmm.edu.mx>
2023-02-26 14:15:50 -0600

    first commit

---
+++ Conversor.py
@@ -0,0 +1,19 @@
+'''
+Raul Antonio Ramirez Mendez
+App para convertir pies a metros usando TKINTER
+23 de Febrero del 2023
+'''
+
+from tkinter import *
+from tkinter import ttk
+
+class Conversor:
+    
+    #Constructor de la clase
+    def __init__(self, raiz):
+        raiz.title('Pies a metros')
+        
+if __name__ == "__main__":
+    print('Este es el archivo principal')
+    print('Nadamas se mostrar si esto es el archivo principal')
+    
