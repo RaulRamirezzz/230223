b47280515a3396d0ce08f4b0ff02580fd98a777d
RaulRamirezzz <za210110207@zapopan.tecmm.edu.mx>
2023-03-02 14:52:28 -0600

    terminado

--- Conversor.py
+++ Conversor.py
@@ -14,6 +14,33 @@ class Conversor:
     def __init__(self, raiz):
         raiz.title('Pies a metros')
         
+        self.pies = StringVar()
+        self.metros = StringVar()
+        
+        mainFrame=ttk.Frame(raiz)
+        mainFrame.grid(column=0, row=0)
+        
+        piesEntry=ttk.Entry(mainFrame, textvariable=self.pies)
+        piesEntry.grid(column=2,row=1)
+        
+        ttk.Label(mainFrame, text="Pies").grid(column=3, row=1)
+        ttk.Label(mainFrame, text="Son equivalentes a: ").grid(column=1, row=2)
+        ttk.Label(mainFrame, textvariable=self.metros).grid(column=2, row=2)
+        ttk.Label(mainFrame, text="Metros").grid(column=3, row=2)
+        
+        ttk.Button(mainFrame, text="Calcular", command=self.calcular).grid(column=3, row=3)
+        
+        #Hacer la operacion presionando enter
+        raiz.bind("<Return>", self.calcular)
+
+    def calcular(self, *args):
+        print("Boton presionado")
+        pieUsuario = self.pies.get()
+        piesFlotante = float(pieUsuario)
+        metros = piesFlotante * 0.3048
+        print("Metros: ", metros)
+        self.metros.set(metros)
+    
 if __name__ == "__main__":
     print('Este es el archivo principal')
     print('Nadamas se mostrar si esto es el archivo principal')
